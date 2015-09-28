b83e55eb214b1610afed328765e7afdc65fcd9c3
yangjiabin <892986030@qq.com>
2015-09-28 15:06:29 +0800

    201509281506

--- JiaFei.java
+++ JiaFei.java
@@ -2,6 +2,6 @@ public class JiaFei{
 	public static void main(String args[]){
 		System.out.println("I am jiafei!");
 		String str = "YangJiabin";
-		System.out.println("..." + str);
+		//System.out.println("..." + str);
 	}
 }
